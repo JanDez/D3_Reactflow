e28a10b3e340eaf7b4f1eb3d8f1fd1a1c8845a62
Jaifran Hernández <jaifranjose@gmail.com>
2022-11-22 18:49:04 -0400

    Add Panel component

---
+++ src/components/Panel.tsx
@@ -0,0 +1,11 @@
+const Panel = () => {
+    return (
+        <div className="control-panel-container">
+            <div className="control-panel">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Panel
